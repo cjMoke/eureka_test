7ac4ad6bd324f845b7f1da80276587082d5c6e1a
cjMoke <476070150@qq.com>
2019-10-17 09:47:24 +0800

    2019/10/16

---
+++ index-gather-store-service/src/main/java/cn/how2j/trend/test/Test1.java
@@ -0,0 +1,11 @@
+package cn.how2j.trend.test;
+
+import java.util.regex.Pattern;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        System.out.println(Pattern.matches(".*gf.*","abcdefgfedcba"));
+    }
+}
